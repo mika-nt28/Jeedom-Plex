12715211387ba6a3bd2402538f5e7659b52edd4d
Halbout Michael <michaelhalbout@gmail.com>
2016-09-29 09:51:27 +0200

    Update plex.ajax.php

--- core/ajax/plex.ajax.php
+++ core/ajax/plex.ajax.php
@@ -1,21 +1,4 @@
 <?php
-
-/* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
-
 try {
     require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
     include_file('core', 'authentification', 'php');
@@ -25,10 +8,18 @@ try {
         throw new Exception(__('401 - Accès non autorisé', __FILE__));
     }
 	if (init('action') == 'getLibrary') {
-		ajax::success(plex::getLibrary());
+		$equipement=eqLogic::byType('plex')[0];
+		if(is_object($equipement))
+			ajax::success($equipement->getLibrary());
+		else
+			ajax::success(false);
     }
-	if (init('action') == 'SearchMedia') {
-		ajax::success(plex::getMedia(init('Filtre'),init('param')));
+	if (init('action') == 'SearchMedia') {	
+		$equipement=eqLogic::byType('plex')[0];
+		if(is_object($equipement))
+			ajax::success($equipement->getMedia(init('Filtre'),init('param')));
+		else
+			ajax::success(false);
 	}
 	if (init('action') == 'UpdateCommande') {
 		$Commande=plex::byId(init('EqId'))->getCmd(null,init('CmdId'));
