ae1c263f83363e4331401e5fc173659af47ea948
Halbout Michael <michaelhalbout@gmail.com>
2016-10-12 14:35:10 +0200

    Update plex.class.php

--- core/class/plex.class.php
+++ core/class/plex.class.php
@@ -26,7 +26,7 @@ class plex extends eqLogic {
 			//if(isset($this->$_client)&&is_object($this->$_client)&&isset(self::$_server)&&is_object(self::$_server)){
 				$server=self::$_server;
 				$server->getPlayerSessions(array($this->getLogicalId()));
-				$this->getCmd(null,'state')->event($this->$_client->getState());
+				$this->getCmd(null,'state')->event($this->_client->getState());
 				//$MediaOffset=$this->getCmd(null,'viewOffset');
 				//$MediaOffset->execute();
 			//}
