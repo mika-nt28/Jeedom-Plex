882a231c49fd73c518f865e85ed9d1ddcfa6405a
Halbout Michael <michaelhalbout@gmail.com>
2016-10-13 09:24:04 +0200

    Update Plex.php

--- core/php/Plex.php
+++ core/php/Plex.php
@@ -1,6 +1,30 @@
 <?php
+/**
+ * Plex Bootstrap
+ *
+ * This is the file to be included in your application and will bootstrap the
+ * rest of what is required.
+ * 
+ * @category php-plex
+ * @package Plex
+ * @author <nickbart@gmail.com> Nick Bartkowiak
+ * @copyright (c) 2013 Nick Bartkowiak
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GNU Public Licence (GPLv3)
+ * @version 0.0.2
+ *
+ * This file is part of php-plex.
+ * 
+ * php-plex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * php-plex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 $phpPlexDir = dirname(__FILE__);
-/*
 // Exception
 require_once(sprintf('%s/Exception/ExceptionInterface.php', $phpPlexDir));
 require_once(sprintf('%s/Exception/ExceptionAbstract.php', $phpPlexDir));
@@ -39,17 +63,37 @@ require_once(sprintf('%s/Client.php', $phpPlexDir));
 require_once(sprintf('%s/Client/ControllerAbstract.php', $phpPlexDir));
 require_once(sprintf('%s/Client/Controller/Navigation.php', $phpPlexDir));
 require_once(sprintf('%s/Client/Controller/Playback.php', $phpPlexDir));
-require_once(sprintf('%s/Client/Controller/Application.php', $phpPlexDir));*/
+require_once(sprintf('%s/Client/Controller/Application.php', $phpPlexDir));
+/**
+ * Bootstrap class for using php-plex to interact with the Plex HTTP API.
+ * 
+ * @category php-plex
+ * @package Plex
+ * @author <nickbart@gmail.com> Nick Bartkowiak
+ * @copyright (c) 2013 Nick Bartkowiak
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GNU Public Licence (GPLv3)
+ * @version 0.0.2.5
+ */
 class PlexApi
 {
-
+	/**
+	 * A list of Plex server machines on the network. This is defined by the 
+	 * instantiating software.
+	 * @var Plex_Server[]
+	 */
 	private static $servers = array();
+	/**
+	 * A list of the Plex client machines on the network This is found upon
+	 * registring of Plex server. The first registered Plex server will go out
+	 * and get the list of available clients and register them accordingly.
+	 * @var Plex_Client[]
+	 */
 	private static $clients = array();
 	private static $token = '';
 	
 	public function getToken($username, $password)
 	{
-		/*$host = "https://plex.tv/users/sign_in.json";
+		$host = "https://plex.tv/users/sign_in.json";
 		$header = array(
 			   'Content-Type: application/xml; charset=utf-8', 
 			   'Content-Length: 0', 
@@ -70,12 +114,37 @@ class PlexApi
 		log::add('plex','debug','Token: '.$data);
 		$curlError = curl_error($process);
 		$json = json_decode($data, true);
-		//$this->token=$json['user']['authentication_token'];
-		config::save('PlexToken',$json['user']['authentication_token'], 'plex');*/
+		config::save('PlexToken',$json['user']['authentication_token'], 'plex');
 	}
+	/**
+	 * Allows an instantiating software to define a list of Plex servers on the
+	 * network. In addition, the first server listed will be used to find the
+	 * list of available clients and will register them accordingly.
+	 *
+	 * @param array $servers An associative array of Plex server machines on the
+	 * network defined thusly:
+	 *
+	 * array (
+	 *     'server-1-name' => array(
+	 *         'address' => '192.168.1.5',
+	 *         'port' => 32400
+	 *     ),
+	 *     'server-2-name' => array(
+	 *         'address' => '192.168.1.10',
+	 *         'port' => 32400
+	 *     )
+	 * )
+	 *
+	 * @uses Plex::$servers
+	 * @uses Plex::registerClients()
+	 * @uses Plex::getServer()
+	 * @uses Plex_Server::getClient()
+	 *
+	 * @return void
+	 */
 	public function registerServers(array $servers)
 	{
-	/*	// Register each server.
+		// Register each server.
 		foreach ($servers as $name => $server) {
 			$port = isset($server['port']) ? $server['port'] : NULL;
 			self::$servers[$name] = new Plex_Server(
@@ -90,22 +159,32 @@ class PlexApi
 		$serverName = reset(array_keys(self::$servers));
 		$this->registerClients(
 			$this->getServer($serverName)->getClients()
-		);*/
+		);
 	}
 	
 	public function UpdateClientStatus()
 	{
-	/*	// We are going to use the first server in the list to get a list of the
+		// We are going to use the first server in the list to get a list of the
 		// availalble clients and register those automatically.
 		$serverName = reset(array_keys(self::$servers));
 		$this->getPlayerSessions(
 			$this->getServer($serverName)->getClients()
-		);*/
+		);
 	}
-
+	/**
+	 * Registers each found client with the bootstrap, so they can be found and
+	 * used by the instantiating software.
+	 *
+	 * @param Plex_Client[] $clients An associative array of Plex client machines on the
+	 * network.
+	 *
+	 * @uses Plex::$clients
+	 *
+	 * @return void
+	 */
    	private function registerClients(array $clients)
 	{
-		//self::$clients = $clients;
+		self::$clients = $clients;
 	}
 	
 	/**
@@ -121,14 +200,13 @@ class PlexApi
 	 */
 	public function getServer($serverName)
 	{
-		/*if (!isset(self::$servers[$serverName])) {
+		if (!isset(self::$servers[$serverName])) {
 			throw new Plex_Exception_Server(
 				'RESOURCE_NOT_FOUND', 
 				array($serverName)
 			);
 		}
-
-		return self::$servers[$serverName];*/
+		return self::$servers[$serverName];
 	}
 	
 	/**
@@ -142,11 +220,11 @@ class PlexApi
 	 */
 	public function getClients()
 	{
-		//return self::$clients;
+		return self::$clients;
 	}
 	public function getClient($clientName)
 	{
-		/*if(isset(self::$clients[$clientName]))
-			return self::$clients[$clientName];*/
+		if(isset(self::$clients[$clientName]))
+			return self::$clients[$clientName];
 	}
 }
