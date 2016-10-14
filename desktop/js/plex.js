cad95e5d479702ab2b46d3446e6371b0649c59f6
Halbout Michael <michaelhalbout@gmail.com>
2016-10-14 08:56:10 +0200

    Update plex.js

--- desktop/js/plex.js
+++ desktop/js/plex.js
@@ -1,19 +1,3 @@
-/* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
-
 $('.eqLogicAttr[data-l1key=logicalId]').on( 'change', function() {
 	if($('.bt_PlexClient').length>0 && $(this).val()!=''){
 		$('.bt_PlexClient').removeClass('btn-primary');
@@ -42,7 +26,7 @@ function addCmdToTable(_cmd) {
     tr += '<td class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType();
     tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span></td>';
     tr += '<td>';
-    tr += '<span><input type="checkbox" data-size="mini" data-label-text="{{Historiser}}" class="cmdAttr bootstrapSwitch" data-l1key="isHistorized" /></span> ';
+    tr += '<span><input type="checkbox" data-size="mini" data-label-text="{{Historiser}}" class="cmdAttr" data-l1key="isHistorized" />{{Historiser}}</span> ';
     tr += '</td>';
     tr += '<td>';
      if (is_numeric(_cmd.id)) {
