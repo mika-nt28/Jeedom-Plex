e1830d256c524640edfeaf5d23322e73f87e8ff9
Halbout Michael <michaelhalbout@gmail.com>
2016-10-18 14:30:20 +0200

    Update Server.php

--- core/php/Server.php
+++ core/php/Server.php
@@ -11,7 +11,7 @@ class Plex_Server extends Plex_MachineAbstract
 		$this->address = $address;
 		$this->port = $port ? $port : self::DEFAULT_PORT;
 		$this->token = $token;
-		log:add('plex','debug','Initialisation server : '.$this->getMachineIdentifier())
+		log:add('plex','debug','Initialisation server : '.$this->getMachineIdentifier());
 		if(isset($this->getMachineIdentifier()))
 			$this->ServerInforamation();
 	}
