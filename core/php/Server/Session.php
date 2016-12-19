2fc6cf01de0ccb3f4168ef3dcc82122f42671fec
Halbout Michael <michaelhalbout@gmail.com>
2016-12-19 11:44:40 +0100

    Update Session.php

--- core/php/Server/Session.php
+++ core/php/Server/Session.php
@@ -14,49 +14,47 @@
 		public function getItems(){
 			$items = array();
 			$this->_ActiveSessions = $this->makeCall($this->buildUrl());
-			//foreach ($this->_ActiveSessions as $attribute) {
-				if (isset($this->_ActiveSessions['type'])) {
-				log::add('plex','debug','type:' .$this->_ActiveSessions['type']);
+			foreach ($this->_ActiveSessions['Video'] as $attribute) {
+				if (isset($attribute['type'])) {
+				log::add('plex','debug','type:' .$attribute['type']);
 					$item = Plex_Server_Library_ItemAbstract::factory(
-						$this->_ActiveSessions['type'],
+						$attribute['type'],
 						$this->name,
 						$this->address,
 						$this->port
 					);
-					$item->setAttributes($this->_ActiveSessions);
+					$item->setAttributes($attribute);
 					$items[] = $item;
 				}
-			//}
+			}
 			return $items;
 		}
 		public function getPlayer($clients){
-			$this->_ActiveSessions = $this->makeCall($this->buildUrl());
-			foreach ($this->_ActiveSessions as $Session) {		
-				foreach ($Session['Player'] as $attribute) {
-					if(isset($clients[$attribute['device']])){
-						$client=$clients[$attribute['device']];
-						if(isset($attribute['machineIdentifier']))
-							$client->setMachineIdentifier($attribute['machineIdentifier']);
-						return true;
-					}else{
-						$port = isset($attribute['port']) ? $attribute['port'] : 3000;
-						$client = new Plex_Client(
-							$attribute['device'],
-							$attribute['address'],
-							$port
-						);
-						$client->setOnlyState(true);
-						$client->setServer($this);
-						if(isset($attribute['host']))
-							$client->setHost($attribute['host']);
-						if(isset($attribute['machineIdentifier']))
-							$client->setMachineIdentifier($attribute['machineIdentifier']);
-						if(isset($attribute['version']))
-							$client->setVersion($attribute['version']);
-					}
+		$this->_ActiveSessions = $this->makeCall($this->buildUrl());
+		foreach ($SessionArray as $Session) {			
+			foreach ($Session['Player'] as $attribute) {
+				if(isset($clients[$attribute['device']]))
+					$client=$clients[$attribute['device']];
+				else{
+					$port = isset($attribute['port']) ? $attribute['port'] : 3000;
+					$client = new Plex_Client(
+						$attribute['device'],
+						$attribute['address'],
+						$port
+					);
+				if(isset($attribute['host']))
+					$client->setHost($attribute['host']);
+				if(isset($attribute['machineIdentifier']))
+					$client->setMachineIdentifier($attribute['machineIdentifier']);
+				if(isset($attribute['version']))
+					$client->setVersion($attribute['version']);
+					$client->setOnlyState(true);
+					$client->setServer($this);
 				}
+				return true;
 			}
-			return false;
 		}
+		return false;
+	}
 	}
 ?>
