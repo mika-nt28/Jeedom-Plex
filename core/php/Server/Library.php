4c79737ceceda3715aaed48f678bbfaed2d13e1e
Halbout Michael <michaelhalbout@gmail.com>
2016-12-21 08:24:27 +0100

    Update Library.php

--- core/php/Server/Library.php
+++ core/php/Server/Library.php
@@ -39,7 +39,7 @@ class Plex_Server_Library extends Plex_Server
 	{
 		$endpoint = str_replace('/library/', '/', $key);
 		$media=$this->getItems($endpoint);
-		return $media[0]->getLibrarySectionId()
+		return $media[0]->getLibrarySectionId();
 		throw new Plex_Exception_Server_Library(
 			'RESOURCE_NOT_FOUND',
 			array('section', $key)
