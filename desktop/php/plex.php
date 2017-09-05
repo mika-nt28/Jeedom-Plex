ecaeaeca257f7d7405be41926591c9b4346bdc01
Halbout Michael <michaelhalbout@gmail.com>
2017-09-05 08:40:19 +0200

    Update plex.php

--- desktop/php/plex.php
+++ desktop/php/plex.php
@@ -56,24 +56,24 @@ $eqLogics = eqLogic::byType('plex');
 		</div>
     </div>
 	<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-	<a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> Sauvegarder</a>
-	<a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> Supprimer</a>
-	<a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> Configuration avancée</a>
-	<ul class="nav nav-tabs" role="tablist">
-		<li role="presentation">
-			<a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay">
-				<i class="fa fa-arrow-circle-left"></i>
-			</a>
-		</li>
-		<li role="presentation" class="active">
-			<a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab" aria-expanded="true">
-				<i class="fa fa-tachometer"></i> Equipement</a>
-		</li>
-		<li role="presentation" class="">
-			<a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false">
-				<i class="fa fa-list-alt"></i> Commandes</a>
-		</li>
-	</ul>
+		<a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> Sauvegarder</a>
+		<a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> Supprimer</a>
+		<a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> Configuration avancée</a>
+		<ul class="nav nav-tabs" role="tablist">
+			<li role="presentation">
+				<a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay">
+					<i class="fa fa-arrow-circle-left"></i>
+				</a>
+			</li>
+			<li role="presentation" class="active">
+				<a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab" aria-expanded="true">
+					<i class="fa fa-tachometer"></i> Equipement</a>
+			</li>
+			<li role="presentation" class="">
+				<a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false">
+					<i class="fa fa-list-alt"></i> Commandes</a>
+			</li>
+		</ul>
 		<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
 			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
 				<div class="col-sm-6">
@@ -145,7 +145,7 @@ $eqLogics = eqLogic::byType('plex');
 						</fieldset> 
 					</form>
 				</div>
-								<div class="col-sm-6">
+				<div class="col-sm-6">
 					<form class="form-horizontal">
 						<legend>Paramètre du clients</legend>
 						<fieldset>
@@ -183,38 +183,25 @@ $eqLogics = eqLogic::byType('plex');
 									<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="volume_inc" placeholder="{{Volume incrément}}"/>
 								</div>
 							</div>						
-								<label class="col-sm-2 control-label" >
-									{{Etat du widget}}
-									<sup>
-										<i class="fa fa-question-circle tooltips" title="Choisissez les options de visibilité et d'activation
-									Si l'equipement n'est pas activé il ne sera pas utilisable dans jeedom, mais visible sur le dashboard
-									Si l'equipement n'est pas visible il ne sera caché sur le dashbord, mais utilisable dans jeedom" style="font-size : 1em;color:grey;"></i>
-									</sup>
-								</label>
-								<div class="col-sm-9">
-									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-								</div>
-							</div>
 						</fieldset> 
 					</form>
 				</div>
 			</div>
-			<div role="tabpanel" class="tab-pane" id="commandtab">
-				<br>
-				<!--a class="btn btn-success btn-sm cmdAction pull-right" data-action="add"><i class="fa fa-plus-circle"></i> Ajouter une commande</a-->
-				<table id="table_cmd" class="table table-bordered table-condensed">
-					<thead>
-						<tr>
-							<th>Nom</th>
-							<th>Type</th>
-							<th>Options</th>
-							<th>Actions</th>
-						</tr>
-					</thead>
-					<tbody></tbody>
-				</table>
-			</div>
+		</div>
+		<div role="tabpanel" class="tab-pane" id="commandtab">
+			<br>
+			<!--a class="btn btn-success btn-sm cmdAction pull-right" data-action="add"><i class="fa fa-plus-circle"></i> Ajouter une commande</a-->
+			<table id="table_cmd" class="table table-bordered table-condensed">
+				<thead>
+					<tr>
+						<th>Nom</th>
+						<th>Type</th>
+						<th>Options</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody></tbody>
+			</table>
 		</div>
 	</div>
 </div>
